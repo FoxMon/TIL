ec5da05deac1d2a6d59ca834d41543d1a24bb9a6
FoxMon <jjockrod@naver.com>
2021-09-11 15:53:31 +0900

    2021.09.11

--- src/baek/queen/Main.java
+++ src/baek/queen/Main.java
@@ -1,7 +1,45 @@
 package baek.queen;
 
-public class Main { // N-Queen
+import java.util.Scanner;
+
+public class Main {
+    public static int count = 0;
+    public static int map;
+    public static int[] queens;
+
     public static void main(String[] args) {
-        //oo
+        Scanner scanner = new Scanner(System.in);
+        map = scanner.nextInt();
+        queens = new int[map];
+
+        backTracking(0);
+        System.out.println(count);
+    }
+
+    public static void backTracking(int level) {
+        if(level >= map) {
+            count++;
+            return;
+        }
+
+        for(int i = 0; i < map; i++) {
+            queens[level] = i;
+
+            if(isPossible(queens, level)) {
+                backTracking(level + 1);
+            }
+        }
+    }
+
+    public static boolean isPossible(int[] queens, int col) {
+        for(int i = 0; i < col; i++) {
+            if(queens[col] == queens[i]) {
+                return false;
+            } else if(Math.abs(col - i) == Math.abs(queens[col] - queens[i])) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
