7df5778383ba6a618cfe0600357dba8f4f3554d8
FoxMon <jjockrod@naver.com>
2021-12-26 21:14:15 +0900

    2021.12.26

---
+++ src/baekString/string01/Main.java
@@ -0,0 +1,17 @@
+package baekString.string01;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+
+        for(int i = 0; i < str.length(); i++) {
+            if(i != 0 && i % 10 == 0) {
+                System.out.println();
+            }
+            System.out.print(str.charAt(i) + "");
+        }
+    }
+}
---
+++ src/baekString/string02/Main.java
@@ -0,0 +1,22 @@
+package baekString.string02;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        String[] arr = new String[str.length()];
+
+        for(int i = 0; i < str.length(); i++) {
+            arr[i] = str.substring(i);
+        }
+
+        Arrays.sort(arr);
+
+        for(String temp : arr) {
+            System.out.println(temp);
+        }
+    }
+}
